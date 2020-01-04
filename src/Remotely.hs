8e2f33f38b4c510700274b25bfc5c7b203db368f
Mark Wotton <mwotton@gmail.com>
2020-01-04 13:36:30 -0500

    skeleton

--- src/Remotely.hs
+++ src/Remotely.hs
@@ -1,6 +1,29 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
 module Remotely
-       ( someFunc
+       ( remotely
+       , RemoteError(..)
+       , Status(..)
        ) where
 
-someFunc :: IO ()
-someFunc = putStrLn ("someFunc" :: String)
+import Data.Serialize
+import Control.Monad.IO.Class(MonadIO)
+import GHC.Generics(Generic)
+
+data Status = Done | NotDone
+  deriving Generic
+instance Serialize Status
+
+data RemoteError
+  = CannotConnect
+  | ParseError
+  | ExecutableError
+  deriving Generic
+
+instance Serialize RemoteError
+
+type Hostname = String
+
+remotely :: (Serialize a, Serialize b, MonadIO m)
+         => Hostname -> FilePath -> (a -> m Status, m (Maybe b))  -> m (Either RemoteError ())
+remotely hostname remotePath (sender,receiver) = undefined
